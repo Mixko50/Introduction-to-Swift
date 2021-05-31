a8d93f4813d5c9aaf30070a0d05229c0d9dcd72b
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-05-31 21:11:23 +0700

    Learning types

---
+++ 02-Types/Types.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//Types
+
+var x = 3 //int
+var name = "Joe" //String
+
+type(of: name) //Check type
+
+//set type
+var lastname: String = "last"
+
+let age: Int = 20
+let price: Double = 33.4
+var height: Float = 3.44343
+
+var isComplete: Bool = false
+isComplete = true
+
+class Person {
+    
+}
+
+var john: Person = Person()
+
+var button1: UIButton = UIButton()
