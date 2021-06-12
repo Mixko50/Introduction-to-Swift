93df240d2d1a154ada6e430b5d6f8a5f327ce4e9
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-06-12 23:07:56 +0700

    Learning Naming Conventions 🍧

---
+++ 16-Naming-Conventions/Naming Conventions.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var number_of_bank_accounts = "" //snake case
+var numberOfBankAccount = "" //camel case
+
+func is_larger_than_five() {}
+func isLargerThanFive() {}
+
+class BigPerson {} //Naming in class usually all capitalized
